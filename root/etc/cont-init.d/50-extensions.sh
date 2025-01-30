227f03eb35fa1bb3b8fd453d8917ea901da5a01c
Antoine Besnier <besnier_antoine@yahoo.fr>
2025-01-30 09:56:19 +0100

    Better clean up of extensions at start up to prevent old extensions to remain when not used

--- root/etc/cont-init.d/50-extensions.sh
+++ root/etc/cont-init.d/50-extensions.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/with-contenv sh
 
+# remove all extensions from previous versions from the extensions directory
 echo "Cleaning Extensions from previous Guacamole versions"
 for e in $(ls -1 ${GUACAMOLE_HOME}/extensions | grep -v ${GUAC_VER}); do
   rm ${GUACAMOLE_HOME}/extensions/${e}
 done
 
+# Remove all provided extensions, they will be populated back when container starts.
 echo "Cleaning Extensions"
 for i in auth-duo auth-header auth-json auth-restrict auth-ldap auth-quickconnect auth-sso-cas auth-sso-openid auth-sso-saml auth-sso-ssl auth-totp branding display-statistics history-recording-storage vault-ksm; do
-  rm -rf ${GUACAMOLE_HOME}/extensions/guacamole-${i}-${GUAC_VER}.jar
+  rm -rf ${GUACAMOLE_HOME}/extensions/guacamole-${i}-*.jar
 done
 
 # this was from Oznu's image
